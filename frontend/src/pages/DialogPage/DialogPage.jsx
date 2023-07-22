52789b4c71f8abc309e2d5c70b7928e93317f0f9
Kostiantyn Ivanov <kostia10kot@gmail.com>
2023-07-22 16:32:29 +0300

    impl: add message loader

--- frontend/src/pages/DialogPage/DialogPage.jsx
+++ frontend/src/pages/DialogPage/DialogPage.jsx
@@ -13,14 +13,18 @@ function DialogPage () {
 
 	const { selectedTheme } = useTheme();
 	const [messages, setMessages] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const userMessage = useInput('');
 
 	async function sendMessage(textMessage, needToClear) {
 		try {
+			setIsLoading(prev => !prev);
 			const response = await axios.post('http://localhost:3001' + '/dialog', { teacher: textMessage, needToClear: needToClear });
 			const message = await response.data.choices[0].message;
 			const newObj = await { ...message, content: message.content.replace(/\n/g, '<br/>')};
 			setMessages(prev => [...prev, newObj]);
+			setIsLoading(prev => !prev);
+			console.log("я тут");
 		} catch (error) {
 			console.log(error)
 		}
@@ -54,7 +58,7 @@ function DialogPage () {
 					<h2 className={globalStyles.pages_title}>
 						Dialogue on the topic of "{selectedTheme}"
 					</h2>
-					<MessageWindow messages={messages} userMessage={userMessage} onSendMessage={submitMessage} />
+					<MessageWindow messages={messages} userMessage={userMessage} onSendMessage={submitMessage} isLoading={isLoading}/>
 					{/* <div className={styles.tags}><div className={styles.tags_item}>Продовжуй поглиблюватися</div></div> */}
 				</div>
 			</div>
--- frontend/src/pages/DialogPage/components/MessageWindow/MessageWindow.jsx
+++ frontend/src/pages/DialogPage/components/MessageWindow/MessageWindow.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useId } from "react";
 import styles from "./message-window.module.scss";
 import MessageItem from "../MessageItem/MessageItem";
 
-const MessageWindow = ({ messages, userMessage, onSendMessage}) => {
+const MessageWindow = ({ messages, userMessage, onSendMessage, isLoading }) => {
 
 	const messagesWindowRef = useRef(null);
+
+	const fakeId = useId();
 
 	const scrollToBottom = () => {
 		if (messagesWindowRef.current) {
@@ -14,14 +16,16 @@ const MessageWindow = ({ messages, userMessage, onSendMessage}) => {
 
 	useEffect(() => {
 		scrollToBottom();
-	}, [messages]);
+	}, [messages, isLoading]);
 
 	return (
 		<div>
 			<div className={styles.messages_window} ref={messagesWindowRef}>
 				{messages.map((content, index) => (
-					<MessageItem role={content?.role} message={{ __html: content?.content }} key={index} />
+					<MessageItem role={content?.role} message={{ __html: content?.content }} key={index} isLoading={isLoading}/>
 				))}
+				{!isLoading ? null :
+					<MessageItem role='assistant' message={{ __html: 'Loading' }} key={fakeId}/>}
 			</div>
 			<div className={styles.textarea_block}>
 				<textarea className={styles.textarea} value={userMessage.value} onChange={userMessage.onChange} />
